38053ef6f97d37d3323a3d4f2c902bf83a613cfa
Sanjai Kumaar M <sanjaikumaar007@gmail.com>
2021-10-14 16:54:00 +0530

    login desgined

---
+++ app/src/main/java/com/example/miniproject/DBhelper.java
@@ -0,0 +1,80 @@
+package com.example.miniproject;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+//import androidx.annotation.Nullable;
+
+public class DBhelper extends SQLiteOpenHelper {
+    public DBhelper(Context context) {
+        super(context,"Userdata.db",null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase DB) {
+        DB.execSQL("create Table adminLogin(userid TEXT primary key,password TEXT)");
+        DB.execSQL("create Table workerLogin(userid TEXT primary key,password TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase DB, int i, int i1) {
+        DB.execSQL("drop table if exists adminLogin");
+        DB.execSQL("drop table if exists workerLogin");
+    }
+    public void insertAdmin(String userid,String password){
+        SQLiteDatabase DB = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("userid","admin");
+        contentValues.put("password","admin");
+        long result = DB.insert("adminLogin",null,contentValues);
+        /*if(result == -1)
+            return false;
+        else
+            return true;*/
+    }
+    public Boolean updateAdmin(String userid,String password){
+        SQLiteDatabase DB = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("userid",userid);
+        contentValues.put("password",password);
+        Cursor cursor = DB.rawQuery("Select * from adminLogin where userid = ?", new String[]{userid});
+        if (cursor.getCount() > 0) {
+            long result = DB.update("adminLogin", contentValues, "userid=?", new String[]{userid});
+            if (result == -1)
+                return false;
+            else
+                return true;
+        }
+        else
+            return false;
+    }
+    public Boolean deletetuserdata(String name) {
+        SQLiteDatabase DB = this.getWritableDatabase();
+        //ContentValues contentValues = new ContentValues();
+        // contentValues.put("name",name);
+        //contentValues.put("contact", contact);
+        //contentValues.put("dob", dob);
+        Cursor cursor = DB.rawQuery("Select * from Userdetails where name=?", new String[]{name});
+        if (cursor.getCount() > 0) {
+            long result = DB.delete("Userdetails", "name=?", new String[]{name});
+            if (result == -1)
+                return false;
+            else
+                return true;
+        }
+        else
+            return false;
+    }
+    public Boolean validateAdmin(String userid,String password) {
+        SQLiteDatabase DB = this.getWritableDatabase();
+        Cursor cursor = DB.rawQuery("Select * from adminLogin", null);
+        while (cursor.moveToNext()) {
+            if (cursor.getString(0).equals("admin") && (cursor.getString(1).equals("password"))) {
+                return true;
+            }
+        }
+        return  false;
+    }
+}
--- app/src/main/java/com/example/miniproject/MainActivity.java
+++ app/src/main/java/com/example/miniproject/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.miniproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button admin,user,worker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        admin = (Button)findViewById(R.id.admin);
+        user = (Button)findViewById(R.id.user);
+        worker = (Button)findViewById(R.id.worker);
+        admin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),AdminLogin.class);
+                startActivity(i);
+
+            }
+        });
     }
 }
