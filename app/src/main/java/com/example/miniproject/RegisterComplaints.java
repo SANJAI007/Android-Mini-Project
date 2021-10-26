35f54da1018781cf1bdff57e932590bd8463b424
Sanjai Kumaar M <sanjaikumaar007@gmail.com>
2021-10-26 23:42:48 +0530

    location got from image and decoded and sent as sms to admin

--- app/src/main/java/com/example/miniproject/RegisterComplaints.java
+++ app/src/main/java/com/example/miniproject/RegisterComplaints.java
@@ -2,10 +2,16 @@ package com.example.miniproject;
 
 import android.content.ContentResolver;
 import android.content.Intent;
+import android.database.Cursor;
+import android.location.Address;
+import android.location.Geocoder;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import androidx.annotation.NonNull;
+
+import android.provider.MediaStore;
 import android.view.View;
 import android.webkit.MimeTypeMap;
 import android.widget.Button;
@@ -28,6 +34,10 @@ import com.google.firebase.storage.StorageTask;
 import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 public class RegisterComplaints extends AppCompatActivity {
 
     private static final int PICK_IMAGE_REQUEST = 1;
@@ -38,6 +48,10 @@ public class RegisterComplaints extends AppCompatActivity {
     private EditText mEditTextFileName;
     private ImageView mImageView;
     private ProgressBar mProgressBar;
+    EditText tv ;
+
+    String lat,lon,latstr,longstr;
+    String address,city;
 
     private Uri mImageUri;
 
@@ -46,15 +60,16 @@ public class RegisterComplaints extends AppCompatActivity {
 
     private StorageTask mUploadTask;
 
+    String filename;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_complaints);
-
+//        tv= (EditText) findViewById(R.id.latlong);
         mButtonChooseImage = findViewById(R.id.button_choose_image);
         mButtonUpload = findViewById(R.id.button_upload);
-        mTextViewShowUploads = findViewById(R.id.text_view_show_uploads);
-        mEditTextFileName = findViewById(R.id.edit_text_file_name);
+       // mTextViewShowUploads = findViewById(R.id.text_view_show_uploads);
+       // mEditTextFileName = findViewById(R.id.edit_text_file_name);
         mImageView = findViewById(R.id.image_view);
         mProgressBar = findViewById(R.id.progress_bar);
 
@@ -79,18 +94,20 @@ public class RegisterComplaints extends AppCompatActivity {
             }
         });
 
-        mTextViewShowUploads.setOnClickListener(new View.OnClickListener() {
+      /*  mTextViewShowUploads.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
             }
-        });
+        });*/
     }
 
     private void openFileChooser() {
-        Intent intent = new Intent();
+        /*Intent intent = new Intent();
         intent.setType("image/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
+        */
+        Intent intent = new Intent(Intent.ACTION_PICK, Uri.parse("content://media/internal/images/media"));
         startActivityForResult(intent, PICK_IMAGE_REQUEST);
     }
 
@@ -101,10 +118,28 @@ public class RegisterComplaints extends AppCompatActivity {
         if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
                 && data != null && data.getData() != null) {
             mImageUri = data.getData();
-
+            filename = getPath(mImageUri);
+            //Toast.makeText(RegisterComplaints.this, filename, Toast.LENGTH_LONG).show();
+//            tv.setText(
+            ReadExif(filename);
             Picasso.with(this).load(mImageUri).into(mImageView);
         }
     }
+
+    public String getPath(Uri mImageUri) {
+        if (mImageUri==null)
+            return null;
+        String[] projection = {MediaStore.Images.Media.DATA};
+        Cursor cur = managedQuery(mImageUri,projection,null,null,null);
+        if(cur != null){
+            int col = cur.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cur.moveToFirst();
+            return  cur.getString(col);
+        }
+        return mImageUri.getPath();
+    }
+
+
 
     private String getFileExtension(Uri uri) {
         ContentResolver cR = getContentResolver();
@@ -112,10 +147,57 @@ public class RegisterComplaints extends AppCompatActivity {
         return mime.getExtensionFromMimeType(cR.getType(uri));
     }
 
+    String ReadExif(String file){
+        String exif="";//""Exif: " + file;
+        try {
+            ExifInterface exifInterface = new ExifInterface(file);
+
+           /* exif += "\nIMAGE_LENGTH: " + exifInterface.getAttribute(ExifInterface.TAG_IMAGE_LENGTH);
+            exif += "\nIMAGE_WIDTH: " + exifInterface.getAttribute(ExifInterface.TAG_IMAGE_WIDTH);
+            exif += "\n DATETIME: " + exifInterface.getAttribute(ExifInterface.TAG_DATETIME);
+            exif += "\n TAG_MAKE: " + exifInterface.getAttribute(ExifInterface.TAG_MAKE);
+            exif += "\n TAG_MODEL: " + exifInterface.getAttribute(ExifInterface.TAG_MODEL);
+            exif += "\n TAG_ORIENTATION: " + exifInterface.getAttribute(ExifInterface.TAG_ORIENTATION);
+            exif += "\n TAG_WHITE_BALANCE: " + exifInterface.getAttribute(ExifInterface.TAG_WHITE_BALANCE);
+            exif += "\n TAG_FOCAL_LENGTH: " + exifInterface.getAttribute(ExifInterface.TAG_FOCAL_LENGTH);
+            exif += "\n TAG_FLASH: " + exifInterface.getAttribute(ExifInterface.TAG_FLASH);
+            exif += "\nGPS related:";
+            exif += "\n TAG_GPS_DATESTAMP: " + exifInterface.getAttribute(ExifInterface.TAG_GPS_DATESTAMP);
+            exif += "\n TAG_GPS_TIMESTAMP: " + exifInterface.getAttribute(ExifInterface.TAG_GPS_TIMESTAMP);*/
+            exif += "\n TAG_GPS_LATITUDE: " + exifInterface.getAttribute(ExifInterface.TAG_GPS_LATITUDE);
+            //exif += "\n TAG_GPS_LATITUDE_REF: " + exifInterface.getAttribute(ExifInterface.TAG_GPS_LATITUDE_REF);
+            exif += "\n TAG_GPS_LONGITUDE: " + exifInterface.getAttribute(ExifInterface.TAG_GPS_LONGITUDE);
+            /*exif += "\n TAG_GPS_LONGITUDE_REF: " + exifInterface.getAttribute(ExifInterface.TAG_GPS_LONGITUDE_REF);
+            exif += "\n TAG_GPS_PROCESSING_METHOD: " + exifInterface.getAttribute(ExifInterface.TAG_GPS_PROCESSING_METHOD);
+
+
+*/         lat = exifInterface.getAttribute(ExifInterface.TAG_GPS_LATITUDE);
+            latstr=lat.replaceAll("/",".");
+            latstr=latstr.replaceAll(",","");
+            lon = exifInterface.getAttribute(ExifInterface.TAG_GPS_LONGITUDE);
+            longstr=lon.replaceAll("/",".");
+            longstr=longstr.replaceAll(",","");
+            latstr=latstr.substring(0,6);
+            longstr=longstr.substring(0,6);
+            //tv.setText(lat);
+           // Toast.makeText(getApplicationContext(),lat+"\n"+latstr.substring(0,6),Toast.LENGTH_SHORT).show();
+         //   Toast.makeText(getApplicationContext(),lon+"\n"+longstr.substring(0,6),Toast.LENGTH_LONG).show();
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            //e.printStackTrace();
+            Toast.makeText(getApplicationContext(),e.toString(),Toast.LENGTH_LONG).show();
+        }
+
+        return exif;
+    }
+
     private void uploadFile() {
         if (mImageUri != null) {
             StorageReference fileReference = mStorageRef.child(System.currentTimeMillis()
                     + "." + getFileExtension(mImageUri));
+            //Toast.makeText(getApplicationContext(),filename,Toast.LENGTH_SHORT).show();
+
 
             mUploadTask = fileReference.putFile(mImageUri)
                     .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
@@ -130,9 +212,10 @@ public class RegisterComplaints extends AppCompatActivity {
                             }, 500);
 
                             Toast.makeText(RegisterComplaints.this, "Upload successful", Toast.LENGTH_LONG).show();
-                            Upload upload = new Upload(mEditTextFileName.getText().toString().trim(),
-                                    //taskSnapshot.getDownloadUrl().toString())
+                            Upload upload = new Upload("",
                                     taskSnapshot.getMetadata().getReference().getDownloadUrl().toString());
+                            //mEditTextFileName.getText().toString().trim(),
+                            //taskSnapshot.getDownloadUrl().toString())
                             String uploadId = mDatabaseRef.push().getKey();
                             mDatabaseRef.child(uploadId).setValue(upload);
                         }
@@ -150,7 +233,25 @@ public class RegisterComplaints extends AppCompatActivity {
                             mProgressBar.setProgress((int) progress);
                         }
                     });
-        } else {
+            try{
+                Geocoder geocoder =new Geocoder(RegisterComplaints.this, Locale.getDefault());
+                List<Address> ad = geocoder.getFromLocation(Double.parseDouble(latstr),Double.parseDouble(longstr),2);
+                address = ad.get(0).getAddressLine(0);
+                 city = ad.get(0).getLocality();
+              //  Toast.makeText(getApplicationContext(),address + "\n" +city +"\n",Toast.LENGTH_LONG ).show();
+
+            }
+            catch (Exception e){
+                e.printStackTrace();  //Latitude: 9.524. Longitude: 77.855 --> Mepco
+                //Toat.makeText
+            }
+            Intent sendIntent = new Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:"+"9597554507"));
+            sendIntent.putExtra("sms_body", "Clean ASAP at Location : "+address+"\n"+city+"\nStatus : Pending");
+            //sendIntent.setType("vnd.android-dir/mms-sms");
+            startActivity(sendIntent);
+
+        }
+        else {
             Toast.makeText(this, "No file selected", Toast.LENGTH_SHORT).show();
         }
     }
